d1c64d83263d05e32f37f407eb85b490e88e7e75
MIGUEL ESTRELLA <miguelangel.estrella.02@gmail.com>
2019-10-01 18:02:37 -0500

    configurar base de datos en el Startup.cs

--- PokeDex/Startup.cs
+++ PokeDex/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using PokeDex.Models;
 
 namespace PokeDex
 {
@@ -30,6 +32,8 @@ namespace PokeDex
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddDbContext<PokemonDbContext>(o => o.UseInMemoryDatabase("Pokedex"));
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
