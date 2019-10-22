c94437b7576b2e4b32adb8d4b112d36e13bcbbe6
Miguel Estrella <miguelangel.estrella.02@gmail.com>
2019-10-22 17:49:05 -0500

    modelo categoria

--- Tienda/Models/Categoria.cs
+++ Tienda/Models/Categoria.cs
@@ -1,7 +1,13 @@
+using System.Collections.Generic;
+
 namespace Tienda.Models
 {
     public class Categoria
     {
-        
+        public int id {get; set;}
+
+        public string Nombre {get;set;}
+
+        public List<Producto> Productos {get; set;}
     }
 }
